f0eb3173ab89a27eed1f8069bd53299bf9a32a4e
bishalkhatiwada <bishal.khatiwada.sibs.edu@gmail.com>
2023-05-08 11:39:53 +0545

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:healthapp/home_page.dart';
 import 'package:healthapp/login_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:device_preview/device_preview.dart';
+import 'package:healthapp/profile_page.dart';
+
+import 'package:sizer/sizer.dart';
 import 'package:healthapp/signup_page.dart';
 
 void main() => runApp(MyApp());
-//   DevicePreview(
-//     enabled: true,
-//     builder: (context) => const MyApp(),
-//   ),
-// );
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -20,13 +17,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(1290, 2796),
-      // minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'First Method',
-          // You can use the library anywhere in the app even in theme
           theme: ThemeData(
             colorSchemeSeed: const Color(0xff5a73d8),
             textTheme: GoogleFonts.plusJakartaSansTextTheme(
@@ -36,9 +31,8 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      // child: const HomePage(),
-      // child: const SignupPage(),
       child: const LoginPage(),
+      // child: ProfilePage(),
     );
   }
 }
